23358ffd41e49c4254265398b13b791dd1ac5e53
Prakhan <prakhan.jsw@gmail.com>
2021-04-27 14:17:53 +0700

    remove comment

--- cores/process.js
+++ cores/process.js
@@ -5,15 +5,15 @@ import {database, database2} from './database'
 let customerModel = database.get().models.customers
 let orderModel = database2.get().models.orders
 
-// CustomersRepository.findOne({ customerNumber: 114 }, { scope: ['customer-orders'] }).then(data => {
-//   console.log('---------- data customer ---------------')
-//   console.log(data);
-// })
+CustomersRepository.findOne({ customerNumber: 114 }, { scope: ['customer-orders'] }).then(data => {
+  console.log('---------- data customer ---------------')
+  console.log(data);
+})
 
-// OrdersRepository.findBy({ customerNumber: 114 }, { limit: 2}).then(data => {
-//   console.log('============= data order -======================')
-//   console.log(data)
-// })
+OrdersRepository.findBy({ customerNumber: 114 }, { limit: 2}).then(data => {
+  console.log('============= data order -======================')
+  console.log(data)
+})
 
 customerModel.findOne({
   where: { id: 114 },
